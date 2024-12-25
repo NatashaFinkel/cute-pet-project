fcfdb4aa83dfe957c8e14bfac15cd63df7c2fee4
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-25 08:43:22 +0100

    Implémentation : fonction generatePetContainer

--- src/pet.ts
+++ src/pet.ts
@@ -8,3 +8,19 @@ export interface Pet {
   description: string;
 }
 
+export function generatePetContainer(pet: Pet) {
+  return `
+ <div class="card" id="card-${pet.id}">
+      <img src="${pet.imgSrc}" alt="${pet.imgAlt}" class="pet-img" />
+      <h2>${pet.name}</h2>
+      <p>${pet.breed}</p>
+      <p>${pet.homeCountry}</p>
+      <p>${pet.description}</p>
+      <div class="x">
+        <button id="right arrow" type="button"></button>
+        <button id="left arrow" type="button"></button>
+      </div>
+    </div>
+`;
+}
+
