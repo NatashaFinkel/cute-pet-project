e964d0895accc8137c9202c3d02be5c612b13d0a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-25 16:12:31 +0100

    Refactorisation : petArray

--- src/pet.ts
+++ src/pet.ts
@@ -29,49 +29,45 @@ export function generatePetContainer(pet: Pet) {
   `;
 }
 
-export const chat: Pet = {
-  id: 'chat',
-  imgSrc: '/assets/chat.jpg',
-  imgAlt: "Image d\'un chat",
-  name: 'Titi',
-  breed: 'chat',
-  description: 'Tellement mignon !',
-};
-
-export const chien: Pet = {
-  id: 'chien',
-  imgSrc: '/assets/chien.jpg',
-  imgAlt: "Image d\'un chien",
-  name: 'Cooper',
-  breed: 'Labrador',
-  description: "J'aime jouer et courir.",
-};
-
-export const tortue: Pet = {
-  id: 'tortue',
-  imgSrc: '/assets/tortue.jpg',
-  imgAlt: "Image d'une tortue",
-  name: 'Caroline',
-  breed: 'Tortue des Galapagos',
-  description: 'Lentenement mais sûrement.',
-};
-
-export const hamster: Pet = {
-  id: 'hamster',
-  imgSrc: '/assets/hamster.jpg',
-  imgAlt: "Image d\'un hamster",
-  name: 'Galilée',
-  breed: 'Hamster doré',
-  description: 'Je suis très malicieux !',
-};
-
-export const lapin: Pet = {
-  id: 'lapin',
-  imgSrc: '/assets/lapin.jpg',
-  imgAlt: "Image d\'un lapin",
-  name: 'Fluffy',
-  breed: 'Lapin nain',
-  description: 'Je suis tout doux !',
-};
-
-export const petArray: Pet[] = [chat, chien, tortue, hamster, lapin];
+export const petArray: Pet[] = [
+  {
+    id: 'chat',
+    imgSrc: '/assets/chat.jpg',
+    imgAlt: "Image d'un chat",
+    name: 'Titi',
+    breed: 'chat',
+    description: 'Tellement mignon !',
+  },
+  {
+    id: 'chien',
+    imgSrc: '/assets/chien.jpg',
+    imgAlt: "Image d'un chien",
+    name: 'Cooper',
+    breed: 'Labrador',
+    description: "J'aime jouer et courir.",
+  },
+  {
+    id: 'tortue',
+    imgSrc: '/assets/tortue.jpg',
+    imgAlt: "Image d'une tortue",
+    name: 'Caroline',
+    breed: 'Tortue des Galapagos',
+    description: 'Lentenement mais sûrement.',
+  },
+  {
+    id: 'hamster',
+    imgSrc: '/assets/hamster.jpg',
+    imgAlt: "Image d'un hamster",
+    name: 'Galilée',
+    breed: 'Hamster doré',
+    description: 'Je suis très malicieux !',
+  },
+  {
+    id: 'lapin',
+    imgSrc: '/assets/lapin.jpg',
+    imgAlt: "Image d'un lapin",
+    name: 'Fluffy',
+    breed: 'Lapin nain',
+    description: 'Je suis tout doux !',
+  },
+];
