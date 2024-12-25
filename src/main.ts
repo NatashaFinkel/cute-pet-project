b0f94f1643fe0546295cee4c037177712f1827b4
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-25 06:46:54 +0100

    Initialisation du projet avec TypeScript et Vite

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import "./style.css";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.ts";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
