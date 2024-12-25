bd52375a0630ef4ea164138cd05560c9bbc5c4ac
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-25 15:51:41 +0100

    Implémentation : affichage aléatoire d'un Pet

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,21 @@ function updatePetContainer(index: number) {
     currentIndex = (currentIndex - 1 + petArray.length) % petArray.length;
     updatePetContainer(currentIndex);
   });
+
+  document.getElementById('surprise-button')?.addEventListener('click', () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * petArray.length);
+    //  Ici,  while crée une boucle qui continue de générer aléatoirement newIndex tant que newIndex === currentIndex, ou si newIndex est l'index qui est juste avant ou juste après currentIndex.
+    } while (
+      newIndex === currentIndex ||
+      newIndex === (currentIndex + 1) % petArray.length ||
+      newIndex === (currentIndex - 1 + petArray.length) % petArray.length
+    );
+
+    currentIndex = newIndex;
+    updatePetContainer(currentIndex);
+  });
 }
 
 updatePetContainer(currentIndex);
--- src/pet.ts
+++ src/pet.ts
@@ -18,6 +18,9 @@ export function generatePetContainer(pet: Pet) {
       <button id="left-arrow" type="button">
         <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> 
       </button>
+      <button id="surprise-button" type="button">
+        <i class="fa-solid fa-question" aria-hidden="true"></i>
+      </button>
       <button id="right-arrow" type="button">
         <i class="fa-solid fa-arrow-right" aria-hidden="true"></i> 
       </button>
