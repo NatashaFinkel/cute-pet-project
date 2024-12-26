294dcec162193b704d83232a603fcfef56b822fb
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-26 18:02:27 +0100

    Implémentation : displayApp et clean code

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,22 @@
 import './style.css';
-import { petArray, generatePetContainer } from './pet.ts';
+import { stickyHeader } from './stickyHeader.ts';
+import updatePetContainer from './updatePetContainer.ts';
 
 let currentIndex = 0;
 
-function updatePetContainer(index: number) {
-  const pet = petArray[index];
-  document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <div>
-      <h1>Adoptez-moi !</h1>
-      ${generatePetContainer(pet)}
-    </div>
-  `;
+function displayApp() {
+  return (document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+    <header id="sticky-header"></header>
+      <div>
+        <h1>Adoptez-moi !</h1>
+      </div>
+     <main>
+      <div id="pet-container"></div>
+     </main>
 
-  document.getElementById('right-arrow')?.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % petArray.length;
-    updatePetContainer(currentIndex);
-  });
-
-  document.getElementById('left-arrow')?.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + petArray.length) % petArray.length;
-    updatePetContainer(currentIndex);
-  });
-
-  document.getElementById('surprise-button')?.addEventListener('click', () => {
-    let newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * petArray.length);
-      //  Ici,  while crée une boucle qui continue de générer aléatoirement newIndex tant que newIndex === currentIndex, ou si newIndex est l'index qui est juste avant ou juste après currentIndex.
-    } while (
-      newIndex === currentIndex ||
-      newIndex === (currentIndex + 1) % petArray.length ||
-      newIndex === (currentIndex - 1 + petArray.length) % petArray.length
-    );
-
-    currentIndex = newIndex;
-    updatePetContainer(currentIndex);
-  });
+   `);
 }
 
+displayApp();
+stickyHeader();
 updatePetContainer(currentIndex);
