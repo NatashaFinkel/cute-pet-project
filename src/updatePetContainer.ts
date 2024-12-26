e6a365c81e7d59a5cce37c096bcd46f3985263ba
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-26 17:57:42 +0100

    Refactorisation : updatePetContainer dans un fichier séparé

---
+++ src/updatePetContainer.ts
@@ -0,0 +1,34 @@
+import { petArray, generatePetContainer } from './pet.ts';
+let currentIndex = 0;
+
+export default function updatePetContainer(index: number) {
+  const pet = petArray[index];
+     document.getElementById('pet-container')!.innerHTML = `
+      ${generatePetContainer(pet)}
+  `;
+
+  document.getElementById('right-arrow')?.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % petArray.length;
+    updatePetContainer(currentIndex);
+  });
+
+  document.getElementById('left-arrow')?.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + petArray.length) % petArray.length;
+    updatePetContainer(currentIndex);
+  });
+
+  document.getElementById('surprise-button')?.addEventListener('click', () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * petArray.length);
+      //  Ici,  while crée une boucle qui continue de générer aléatoirement newIndex tant que newIndex === currentIndex, ou si newIndex est l'index qui est juste avant ou juste après currentIndex.
+    } while (
+      newIndex === currentIndex ||
+      newIndex === (currentIndex + 1) % petArray.length ||
+      newIndex === (currentIndex - 1 + petArray.length) % petArray.length
+    );
+
+    currentIndex = newIndex;
+    updatePetContainer(currentIndex);
+  });
+}
