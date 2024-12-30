ee62a9eab525e1a13cec06d1c75b56e4d0d087e3
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-30 09:38:06 +0100

    Implémentation : icônes de navigation pour les versions "téléphone"

--- src/stickyHeader.ts
+++ src/stickyHeader.ts
@@ -5,15 +5,21 @@ export function stickyHeader() {
   if (app && headerElement) {
     return (headerElement.innerHTML = `
       <nav class="sticky-header">
-        <div id="large-devices-logo" class="logo-div">
+        <div id="large-devices-logo" class="logo-div large-screen-nav-bar-icon">
           <img src="/assets/logo.png" class="logo-img">
         </div>
         <ul>
           <li class='nav-div'>
-            <a href="#about-us-container">Notre association</a>
+            <a href="#about-us-container" class="large-screen-nav-bar-icon">Notre association</a>
+            <a href="#about-us-container">
+              <i class="fa-solid fa-house small-screen-nav-bar-icon"></i>
+            </a>
           </li>
           <li class='nav-div'>
-            <a href="#pet-container">Nos animaux</a>
+            <a href="#pet-container" class="large-screen-nav-bar-icon">Nos animaux</a>
+            <a href="#pet-container">
+            <i class="fa-solid fa-paw small-screen-nav-bar-icon"></i>
+            </a>
           </li>
         </ul>
       </nav>
