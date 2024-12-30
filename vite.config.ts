e23dbb00b2a6a17dfc826b6fd11783f278d5606f
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-30 14:52:19 +0100

    Feature : Ajout configuration pour le déployement

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [],
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
+  base: '/cute-pet-project',
 });
