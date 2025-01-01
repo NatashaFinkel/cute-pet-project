0eb2ff110efa95d52d8b8b1e9eef470628a8bde9
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-01 12:01:57 +0100

    Fix accessibilité : ajout aria-labels sur les trois boutons de getAnotherPetBtnBar()

--- src/getAnotherPetBtnBar.ts
+++ src/getAnotherPetBtnBar.ts
@@ -2,12 +2,13 @@ export default function getAnotherPetBtnBar() {
   return `
       <div class="button-div">
         <button id="left-arrow" type="button" data-testid="left-arrow">
+        <button id="left-arrow" type="button" aria-label="get the previous pet button">
           <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> 
         </button>
-        <button id="surprise-button" type="button">
+        <button id="surprise-button" type="button" aria-label="get a random pet button">
           <i class="fa-solid fa-question" aria-hidden="true"></i>
         </button>
-        <button id="right-arrow" type="button">
+        <button id="right-arrow" type="button" aria-label="get the next pet button">
           <i class="fa-solid fa-arrow-right" aria-hidden="true"></i> 
         </button>
       </div>
