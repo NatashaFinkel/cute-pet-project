79b16200465ddadcee556b542407c6e38fdacf9a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-01 18:44:54 +0100

    Updates

--- assets/index-Dt-U1sTg.js
+++ assets/index-Dt-U1sTg.js
@@ -3,18 +3,18 @@
   `}function u(){const i=document.querySelector("#app"),n=document.getElementsByTagName("header")[0];if(i&&n)return n.innerHTML=`
       <nav class="sticky-header">
         <div id="large-devices-logo" class="logo-div large-screen-nav-bar-icon">
-          ${l("/assets/logo.png","logo","logo-img")}
+          ${l("/assets/logo.png","logo de Midnight lucky charm","logo-img")}
         </div>
         <ul>
           <li class='nav-div'>
-            <a href="#about-us-container" class="large-screen-nav-bar-icon">Notre association</a>
-            <a href="#about-us-container">
+            <a href="#about-us-container" aria-label="lien vers about-us-container" class="large-screen-nav-bar-icon">Notre association</a>
+            <a href="#about-us-container" aria-label="icône du lien vers about-us-container">
               <i class="fa-solid fa-house small-screen-nav-bar-icon"></i>
             </a>
           </li>
           <li class='nav-div'>
             <a href="#pet-container" class="large-screen-nav-bar-icon">Nos animaux</a>
-            <a href="#pet-container">
+            <a href="#pet-container" aria-label="icône du lien vers pet-container">
             <i class="fa-solid fa-paw small-screen-nav-bar-icon"></i>
             </a>
           </li>
@@ -53,17 +53,17 @@
     ${m()} 
 `}const s=[{imgSrc:"/assets/chat.jpg",imgAlt:"Image d'un chat de gouttière",name:"Titi",breed:"chat de gouttière",description:"Tellement mignon !"},{imgSrc:"/assets/chat-siamois.jpg",imgAlt:"Image d'un chat siamois",name:"Mia",breed:"Chat siamois",description:"On m'appelle jolis yeux."},{imgSrc:"/assets/chien.jpg",imgAlt:"Image d'un labrador",name:"Cooper",breed:"Labrador",description:"J'aime jouer et courir."},{imgSrc:"/assets/dalmatien.jpg",imgAlt:"Image d'un dalmatien",name:"Dotty",breed:"Dalmatien",description:"101 raisons de m'adopter !"},{imgSrc:"/assets/tortue.jpg",imgAlt:"Image d'une tortue",name:"Caroline",breed:"Tortue des Galapagos",description:"Lentement mais sûrement."},{imgSrc:"/assets/hamster.jpg",imgAlt:"Image d'un hamster",name:"Galilée",breed:"Hamster doré",description:"Je suis très malicieux !"},{imgSrc:"/assets/lapin.jpg",imgAlt:"Image d'un lapin",name:"Fluffy",breed:"Lapin nain",description:"Je suis tout doux !"},{imgSrc:"/assets/herisson.jpeg",imgAlt:"Image d'un hérisson",name:"Sonic",breed:"Hérisson",description:"Je suis très rapide."},{imgSrc:"/assets/poisson-rouge.jpg",imgAlt:"Image d'un poisson rouge",name:"Titan",breed:"Poisson rouge",description:"J'y pense, et puis j'oublie !"},{imgSrc:"/assets/poisson-jaune.jpg",imgAlt:"Image d'un poisson jaune",name:"GoldieLocks",breed:"Poisson jaune",description:"Je ne gagne jamais à cache-cache."},{imgSrc:"/assets/perroquet.jpg",imgAlt:"Image d'un perroquet",name:"Polly",breed:"Perroquet",description:"Je répète tout ce que vous dites !"},{imgSrc:"/assets/poule.jpg",imgAlt:"Image d'une poule",name:"Cocotte",breed:"Poule",description:"Je ponds des oeufs en chocolat !"},{imgSrc:"/assets/chevre.jpg",imgAlt:"Image d'une chèvre",name:"Biquette",breed:"Chèvre",description:"Je suis la reine de la montagne !"},{imgSrc:"/assets/zebre.jpg",imgAlt:"Image d'un zèbre",name:"Zozo",breed:"Zèbre",description:"Ze zozotte un peu..."},{imgSrc:"/assets/girafe.jpg",imgAlt:"Image d'une girafe",name:"Ruby",breed:"Girafe",description:"Utile pour espionner vos voisins !"},{imgSrc:"/assets/taupe.jpg",imgAlt:"Image d'une taupe",name:"Todd",breed:"Taupe",description:"Déjà 854 tunnels à mon actif !"}];function p(){return`
       <div class="button-div">
-        <button id="left-arrow" type="button" data-testid="left-arrow">
+        <button id="left-arrow" type="button" aria-label="get the previous pet button">
           <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> 
         </button>
-        <button id="surprise-button" type="button">
+        <button id="surprise-button" type="button" aria-label="get a random pet button">
           <i class="fa-solid fa-question" aria-hidden="true"></i>
         </button>
-        <button id="right-arrow" type="button">
+        <button id="right-arrow" type="button" aria-label="get the next pet button">
           <i class="fa-solid fa-arrow-right" aria-hidden="true"></i> 
         </button>
       </div>
-    `}function f(i){return`
+    `}function h(i){return`
        <h2>Adoptez-moi !</h2>
       ${l(`${i.imgSrc}`,`${i.imgAlt}`,"pet-img")}
       <h3>${i.name}</h3>
@@ -71,14 +71,14 @@
       <p>${i.description}</p>
       ${p()}
     `}let o=0;function d(i){var a;const n=s[i];document.getElementById("pet-container").innerHTML=`
-    ${f(n)}
-  `;function r(e,t){var c;(c=document.getElementById(e))==null||c.addEventListener("click",()=>{o=(o+t+s.length)%s.length,d(o)})}r("right-arrow",1),r("left-arrow",-1),(a=document.getElementById("surprise-button"))==null||a.addEventListener("click",()=>{let e;do e=Math.floor(Math.random()*s.length);while(e===o||e===(o+1)%s.length||e===(o-1+s.length)%s.length);o=e,d(o)})}let h=0;function v(){return document.querySelector("#app").innerHTML=`
+    ${h(n)}
+  `;function r(e,t){var c;(c=document.getElementById(e))==null||c.addEventListener("click",()=>{o=(o+t+s.length)%s.length,d(o)})}r("right-arrow",1),r("left-arrow",-1),(a=document.getElementById("surprise-button"))==null||a.addEventListener("click",()=>{let e;do e=Math.floor(Math.random()*s.length);while(e===o||e===(o+1)%s.length||e===(o-1+s.length)%s.length);o=e,d(o)})}let f=0;function b(){return document.querySelector("#app").innerHTML=`
     <header></header>
      <main>
       <div id="" class="logo-div small-screen-nav-bar-icon main-logo-div">
-       ${l("/assets/logo.png","logo","logo-img")}
+       ${l("/assets/logo.png","logo de Midnight lucky charm","logo-img")}
         </div>
      <div class="container" id="about-us-container"></div>
       <div class="container card" id="pet-container"></div>
      </main>
-   `}v();u();g();d(h);
+   `}b();u();g();d(f);
